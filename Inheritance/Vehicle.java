9e0edc604cb5c50a1362c1252640d0d91b4fe103
Rounakathor <rounakrathor193@gmail.com>
2025-12-03 17:44:14 +0530

    Added Vehicle.java and deleted Animal.java from inheritance

---
+++ Inheritance/Vehicle.java
@@ -0,0 +1,26 @@
+// Write a Java program to create a class called Vehicle with a method called drive(). Create a subclass called Car that overrides the drive() method to print "Repairing a car". 
+class Vehicle {
+
+    void drive(){
+        System.out.println("Vehicle need to be maintained");
+
+    }
+
+
+     public static void main(String[] args) {
+        Vehicle v = new Vehicle();
+        v.drive();
+
+        Car c = new Car();
+        c.drive();
+    }
+    
+}
+
+class Car extends Vehicle{
+    @Override
+    void drive(){
+        System.out.println("Repairing a car");
+    }
+}
+
